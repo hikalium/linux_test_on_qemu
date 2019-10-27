1764c5d8b364945cc7db402ccc6496c3228b7c45
hikalium <hikalium@hikalium.com>
2019-10-27 15:03:08 +0900

    Update ndckpt

--- ndckpt/ndckpt.c
+++ ndckpt/ndckpt.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include <sys/prctl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
+#define PR_ENABLE_NDCKPT 57
+// int prctl(PR_ENABLE_NDCKPT, 0 or restore object id, 0, 0, 0);
+
+static int LaunchPersistentProcessInBackground(const char *path, char *argv[]) {
   pid_t fork_pid = fork();
-  if(fork_pid == 0){
-    puts("Hello from child");
-    printf("  my pid is %d\n", getpid());
-    puts("  loop forever...");
-    for(;;){
-    
+  if (fork_pid == 0) {
+    int result = prctl(PR_ENABLE_NDCKPT, 0, 0, 0, 0);
+    if (result) {
+      perror("ndckpt: prctl(PR_ENABLE_NDCKPT, ...)");
+    } else {
+      printf("ndckpt: prctl(PR_ENABLE_NDCKPT, ...): success\n");
     }
-    return 0;
-  } else if(fork_pid < 0){
+    execve(path, argv, NULL);
+    perror("ndckpt");
+    return 1;
+  } else if (fork_pid < 0) {
     printf("Parent: Fork FAILED! retv=%d\n", fork_pid);
+    perror("ndckpt");
+    return 1;
   }
-  printf("Parent: Forked! child pid=%d\n", fork_pid);
+  printf("Parent: Forked! child pid= %d\n", fork_pid);
+  printf("Parent: wait...\n");
   int child_retv;
   pid_t wait_pid = wait(&child_retv);
-  printf("Parent: wait done! child pid=%d, child retv=%d\n", wait_pid, child_retv);
+  printf("Parent: wait done! child pid=%d, child retv=%d\n", wait_pid,
+         child_retv);
 
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc >= 3 && strcmp(argv[1], "run") == 0) {
+    return LaunchPersistentProcessInBackground(argv[2], &argv[3]);
+  }
+  puts("ndckpt run <path_to_bin>    # run as persistent process");
   return 0;
 }
