49c2154ac48eaee71149a808ee459a3f8826076b
hikalium <hikalium@hikalium.com>
2019-12-30 05:52:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import pexpect
 import time
 import itertools
@@ -10,23 +11,31 @@ import sys
 import json
 import parse
 
+if len(sys.argv) < 2:
+    print('test.py <command_on_remote> [count]', file=sys.stderr);
+    sys.exit(0)
+
+if len(sys.argv) < 3:
+    num_of_tries = 3
+else:
+    num_of_tries = int(sys.argv[2])
+
 def launch_sol():
     p = pexpect.spawn("/home4/hikalium/SMCIPMITool_2.22.0_build.190701_bundleJRE_Linux_x64/SMCIPMITool 192.168.4.112 ADMIN ADMIN sol activate")
     p.expect(r"exit")
-    print('SOL launched')
+    print('SOL launched', file=sys.stderr)
     return p
 
 def read_time(dst, type_str):
     p.expect(type_str + r'\t(\d| |\.)+m(\d| |\.)+s')
     s = p.after.decode("utf-8");
     dst[type_str + '_str'] = s;
-    print(s);
+    print(s, file=sys.stderr);
     parsed = parse.parse(type_str + '\t{:d}m {:f}s', s);
     dst[type_str] = (parsed[0] * 60 + parsed[1]);
     return dst[type_str]
 
 def test_pi(p, script_path):
-    num_of_tries = 3
     result = {
         'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'target': script_path,
@@ -40,15 +49,15 @@ def test_pi(p, script_path):
     for i in range(num_of_tries):
         p.sendline("\ntime " + script_path)
         p.expect(script_path)
-        print("process launched");
+        print("process launched: " + script_path, file=sys.stderr);
         this_try = {}
         sanity = []
         for x in range(10):
             p.expect(r"(OK|NG)")
-            print(p.after.decode("utf-8"), end = " ", flush=True);
+            print(p.after.decode("utf-8"), end = " ", flush=True, file=sys.stderr);
             sanity.append(p.after.decode("utf-8"))
         this_try['sanity'] = sanity;
-        print("") 
+        print("", file=sys.stderr) 
         real_sum = real_sum + read_time(this_try, 'real');
         user_sum = user_sum + read_time(this_try, 'user');
         sys_sum = sys_sum + read_time(this_try, 'sys');
@@ -63,4 +72,4 @@ def test_pi(p, script_path):
 
 if __name__ == "__main__":
     p = launch_sol()
-    test_pi(p, "./test_pi15000.sh")
+    test_pi(p, sys.argv[1])
