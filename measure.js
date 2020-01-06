3226aa173d057f0ceaf292099c5a764795b5c8b9
hikalium <hikalium@hikalium.com>
2020-01-06 22:19:03 +0900

    Update measure

---
+++ measure.js
@@ -0,0 +1,89 @@
+'use strict';
+const {spawn} = require('child_process');
+const {promisify} = require('util');
+
+const localISOString = function() {
+  const d = new Date(),
+        pad =
+            function(n) {
+          return n < 10 ? '0' + n : n;
+        },
+        tz = d.getTimezoneOffset()  // mins
+      ,
+        tzs = (tz > 0 ? '-' : '+') + pad(parseInt(Math.abs(tz / 60)));
+
+  if (tz % 60 != 0) tzs += pad(Math.abs(tz % 60));
+
+  if (tz === 0)  // Zulu time == UTC
+    tzs = 'Z';
+
+  return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-' +
+      pad(d.getDate()) + 'T' + pad(d.getHours()) + ':' + pad(d.getMinutes()) +
+      ':' + pad(d.getSeconds()) + tzs;
+};
+
+function spawnCommand(cmd, args, callback) {
+  console.log(`run: ${cmd + ' ' + args.join(' ')}`)
+  var command = spawn(cmd, args);
+  var result = '';
+  command.stdout.on('data', (data) => {
+    result += data.toString();
+  });
+  command.stderr.on('data', (data) => {
+    console.log(data.toString());
+  });
+  command.on('error', (err) => {
+    return callback(err);
+  });
+  command.on('close', (code) => {
+    return callback(null, JSON.parse(result));
+  });
+}
+
+const run = promisify(spawnCommand);
+
+const run_test = async (target_env, num_of_tries, cmd) => {
+  const result = await run(
+      'python3',
+      ['test.py', '-c', num_of_tries, '-t', target_env, cmd]);
+  return result.real_ave;
+}
+
+async function main() {
+  const target_app = '/bin/pi15000.bin';
+  const num_of_tries = 3;
+  const target_env = 'real';
+  const interval_list = [
+    0,
+    //8,
+    //16,
+    32,
+    64,
+    128,
+    256,
+    512,
+  ];
+  const result_table = [
+    ['date', localISOString(new Date())],
+    ['target_app', target_app],
+    ['num_of_tries', num_of_tries],
+    ['target_env', target_env],
+    ['no_ckpt [s]', await run_test(target_env, num_of_tries, target_app)],
+    [],
+    ['ckpt interval [ms]', 'real [s]'],
+  ];
+  for (const interval_ms of interval_list) {
+    const result = await run('python3', [
+      'test.py', '-c', num_of_tries, '-t', target_env,
+      `ndckpt run ${interval_ms} ${target_app}`
+    ]);
+    result_table.push([interval_ms, result.real_ave]);
+  }
+  console.log('RESULT:');
+  for (const row of result_table) {
+    console.log(row.join(', '));
+  }
+}
+
+main();
+
--- test.py
+++ test.py
@@ -10,32 +10,32 @@ import datetime
 import sys
 import json
 import parse
-
-if len(sys.argv) < 2:
-    print('test.py <command_on_remote> [count]', file=sys.stderr);
-    sys.exit(0)
-
-if len(sys.argv) < 3:
-    num_of_tries = 3
-else:
-    num_of_tries = int(sys.argv[2])
+import argparse
 
 def launch_sol():
     p = pexpect.spawn("/home4/hikalium/SMCIPMITool_2.22.0_build.190701_bundleJRE_Linux_x64/SMCIPMITool 192.168.4.112 ADMIN ADMIN sol activate")
     p.expect(r"exit")
-    print('SOL launched', file=sys.stderr)
+    print('SOL launched', file=sys.stderr, flush=True, end="")
+    return p
+def launch_qemu_serial():
+    p = pexpect.spawn("telnet localhost 1235")
+    p.expect(r"Connected")
+    print('QEMU serial connected', file=sys.stderr, flush=True, end="")
+    p.sendline();
+    p.expect(r"/ #")
+    print('Prompt found', file=sys.stderr, flush=True, end="")
     return p
 
 def read_time(dst, type_str):
     p.expect(type_str + r'\t(\d| |\.)+m(\d| |\.)+s')
     s = p.after.decode("utf-8");
     dst[type_str + '_str'] = s;
-    print(s, file=sys.stderr);
+    print(s, file=sys.stderr, flush=True, end='');
     parsed = parse.parse(type_str + '\t{:d}m {:f}s', s);
     dst[type_str] = (parsed[0] * 60 + parsed[1]);
     return dst[type_str]
 
-def test_pi(p, script_path):
+def test_pi(p, script_path, num_of_tries):
     result = {
         'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'target': script_path,
@@ -47,17 +47,28 @@ def test_pi(p, script_path):
     user_sum = 0
     sys_sum = 0
     for i in range(num_of_tries):
-        p.sendline("\ntime " + script_path)
-        p.expect(script_path)
-        print("process launched: " + script_path, file=sys.stderr);
+        marker = str(int(time.time()));
+        p.sendline("");
+        p.sendline("echo " + marker);
+        p.expect(marker)  # Read out echo
+        print('Marker found', file=sys.stderr, flush=True, end="")
+        p.expect('\r\n')
+
+        cmd = "time sh -c '" + script_path + " && echo END_OF_TESTRUN'";
+        p.sendline(cmd)
+        print("process launched: " + cmd, file=sys.stderr, flush=True, end="");
         this_try = {}
         sanity = []
-        for x in range(10):
-            p.expect(r"(OK|NG)")
-            print(p.after.decode("utf-8"), end = " ", flush=True, file=sys.stderr);
+        while True:
+            idx = p.expect(["OK", "NG", "echo END_OF_TESTRUN", "END_OF_TESTRUN"])
+            if idx == 3:
+                break
+            if idx == 2:
+                continue
+            print(p.after.decode("utf-8"), end = "", flush=True, file=sys.stderr);
             sanity.append(p.after.decode("utf-8"))
+        print("exited", file=sys.stderr, flush=True, end="") 
         this_try['sanity'] = sanity;
-        print("", file=sys.stderr) 
         real_sum = real_sum + read_time(this_try, 'real');
         user_sum = user_sum + read_time(this_try, 'user');
         sys_sum = sys_sum + read_time(this_try, 'sys');
@@ -69,7 +80,20 @@ def test_pi(p, script_path):
     result['sys_ave'] = sys_sum / num_of_tries;
 
     print(json.dumps(result, indent=2))
+    print("done\n", file=sys.stderr, flush=True, end="") 
+    p.expect(r"/ #")
+    print('Prompt found', file=sys.stderr, flush=True, end="")
 
 if __name__ == "__main__":
-    p = launch_sol()
-    test_pi(p, sys.argv[1])
+    parser = argparse.ArgumentParser(description='NDCKPT evaluator')
+    parser.add_argument('command', metavar='CMD',
+                        help='Command to evaluate')
+    parser.add_argument('-c', '--count', type=int, dest='count', default=3, help='Iteration count. default = %(default)s')
+    parser.add_argument('-t', dest='target', choices=['qemu', 'real'], default='qemu', help='Target machine')
+    args = parser.parse_args()
+
+    if args.target == 'qemu':
+        p = launch_qemu_serial()
+    else:
+        p = launch_sol()
+    test_pi(p, args.command, args.count)
